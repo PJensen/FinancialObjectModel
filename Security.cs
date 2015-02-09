b0fdef25feb4ede8d22cc58f4b30f85bfa404728
PJensen <jensen.petej@gmail.com>
2015-02-09 13:58:40 -0500

    added Security and Equity.

---
+++ Security.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace FinancialObjectModel
+{
+	/// <summary>
+	///   Security
+	/// </summary>
+	[Serializable]
+	public abstract class Security
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Security"/> class.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		/// <param name="ticker">The ticker.</param>
+		protected Security(string name, string ticker)
+		{
+			Name = name;
+			Ticker = ticker;
+		}
+
+		/// <summary>
+		///   Gets or sets the ticker.
+		/// </summary>
+		/// <value> The ticker. </value>
+		[XmlAttribute]
+		public string Ticker { get; set; }
+
+		#region ISecurity Members
+
+		/// <summary>
+		///   Gets or sets the name of the security.
+		/// </summary>
+		/// <value> The name. </value>
+		[XmlAttribute]
+		public string Name { get; set; }
+
+		#endregion
+
+		/// <summary>
+		/// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+		/// </returns>
+		/// <filterpriority>2</filterpriority>
+		public override string ToString()
+		{
+			return string.Format("{0}", Name);
+		}
+	}
+}
+
